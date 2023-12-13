c2088cd04ed0eedd9a53d08d4a3035574b2963c9
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2023-12-13 23:48:15 +0800

    feat: intent gallery

--- app/src/main/java/com/ingridentify/ui/add/AddFragment.kt
+++ app/src/main/java/com/ingridentify/ui/add/AddFragment.kt
@@ -2,22 +2,27 @@ package com.ingridentify.ui.add
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.ingridentify.R
 import com.ingridentify.databinding.FragmentAddBinding
+import com.ingridentify.ui.ViewModelFactory
 
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<AddViewModel> { ViewModelFactory.getInstance() }
 
     private val requestPermission = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -31,6 +36,12 @@ class AddFragment : Fragment() {
             ).show()
         }
     }
+
+    private val galleryLauncher = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { it?.let { uri ->
+        viewModel.setImageUri(uri)
+    }}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -49,6 +60,14 @@ class AddFragment : Fragment() {
                 startCamera()
             }
         }
+
+        binding.btnGallery.setOnClickListener {
+            galleryLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+        viewModel.imageUri.observe(requireActivity()) { it?.let { uri: Uri ->
+            binding.ivThumbnail.setImageURI(uri)
+        }}
     }
 
     private fun checkPermission(): Boolean = ContextCompat.checkSelfPermission(
